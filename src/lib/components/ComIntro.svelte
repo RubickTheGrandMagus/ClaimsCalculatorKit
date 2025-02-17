3bbeec479d39326a800c2509d79973c66f580d30
PickleRick <40783878+RubickTheGrandMagus@users.noreply.github.com>
2025-02-18 07:28:12 +0800

    added documentary requirements. not yet done

--- src/lib/components/ComIntro.svelte
+++ src/lib/components/ComIntro.svelte
@@ -3,4 +3,4 @@
 "Upon attaining fifty-six (56) years of age with at least ten (10) years of active service, an officer or on-officer shall be compulsorily retired under this Resolution: Provided, that an officer with a rank equivalent to Chief Superintendent or higher may, upon the recommendation of the Chief, BFP be allowed service extension for a non-extendable period of one (1) year by the Secretary, DILG with the approval of the Civil Service Commission."
 <b>-RESOLUTION NO. 2001-01 Section 8.2.1</b> 
 </blockquote>
-<p>Please refer to this link for <a href="/docs" class="link link-info">documentary requirements</a>.</p>
+<p>Please refer to this link for <a href="/docs/compulsory" class="link link-info">documentary requirements</a>.</p>
---
+++ src/routes/docs/compulsory/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+
+    let { data }: { data: PageData } = $props();
+</script>
+
+<div class="collapse collapse-plus bg-base-200 collapse-open">
+    <input type="radio" name="my-accordion-3" />
+    <div class="collapse-title text-xl font-medium">Retirement Gratuity Claim (3 yr Lumpsum / Outright Pension)</div>
+    <div class="collapse-content">
+        <p>1 Folder containing 2 copies of the following:</p>
+        <span class="font-bold text-xl">CLAIMANT'S REQUIREMENTS</span>
+        <details class="collapse bg-base-200 hover:collapse-open">
+            <summary class="collapse-title text-xl font-medium">A. Letter of Intent addressed to the Regional Director</summary>
+            <div class="collapse-content">
+              <ul>
+                <li class="hover:font-bold">Watch Video Tutorial</li>
+                <li>Download File</li>
+              </ul>
+            </div>
+        </details>
+    </div>
+  </div>
+<div class="collapse collapse-plus bg-base-200">
+    <input type="radio" name="my-accordion-3" />
+    <div class="collapse-title text-xl font-medium">Terminal Leave Claim</div>
+    <div class="collapse-content">
+      <p>hello</p>
+    </div>
+</div>
