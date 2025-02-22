e01f820650d5b02a3fd4bcb2f2848e15d392f4bf
PickleRick <40783878+RubickTheGrandMagus@users.noreply.github.com>
2025-02-23 07:02:44 +0800

    part 2 of documentanry requirements

--- src/routes/docs/compulsory/+page.svelte
+++ src/routes/docs/compulsory/+page.svelte
@@ -4,20 +4,92 @@
     let { data }: { data: PageData } = $props();
 </script>
 
-<div class="collapse collapse-plus bg-base-200 collapse-open">
+<div class="collapse collapse-plus bg-base-200">
     <input type="radio" name="my-accordion-3" />
     <div class="collapse-title text-xl font-medium">Retirement Gratuity Claim (3 yr Lumpsum / Outright Pension)</div>
     <div class="collapse-content">
+        <a href="#" class="font-bold text-blue-600 hover:underline">Download Table of Contents</a>
         <p>1 Folder containing 2 copies of the following:</p>
-        <span class="font-bold text-xl">CLAIMANT'S REQUIREMENTS</span>
         <details class="collapse bg-base-200 hover:collapse-open">
             <summary class="collapse-title text-xl font-medium">A. Letter of Intent addressed to the Regional Director</summary>
             <div class="collapse-content">
               <ul>
-                <li class="hover:font-bold">Watch Video Tutorial</li>
+                <li> <a href="#" class="font-bold text-blue-600 hover:underline">Watch Video Tutorial</a></li>
                 <li>Download File</li>
               </ul>
             </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">B. Latest Personnal Data Sheet with Picture</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li>Download File</li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">C. Duly filled-out ATM Account Information and Photocopy of ATM</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li>Download File</li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">D. Ombudsman Clearance – Original Copy (1) Certified True Copy (1)</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li>Download File</li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">E. Declaration of Pendency/Non-pendency of Case from Civil Service Commission – Original Copy (1) Certified True Copy (1)</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li>Download File</li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">F. Retirement Order</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li>Download File</li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">G. Updated Statement of Service</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li>Download File</li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">H. Photocopy of Original (FO1) and Latest Appointment</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li>Download File</li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">I. Certificate of Non-pending Case (RIAS Clearance) with valid BFP ID</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li>Download File</li>
+            </ul>
+          </div>
         </details>
     </div>
   </div>
