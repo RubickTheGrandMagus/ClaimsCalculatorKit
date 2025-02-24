0332fda24de66f5706a3eba0a5495b58bc9b0601
PickleRick <40783878+RubickTheGrandMagus@users.noreply.github.com>
2025-02-25 05:35:55 +0800

    part 3 of documentary requirements

--- src/routes/docs/compulsory/+page.svelte
+++ src/routes/docs/compulsory/+page.svelte
@@ -8,14 +8,14 @@
     <input type="radio" name="my-accordion-3" />
     <div class="collapse-title text-xl font-medium">Retirement Gratuity Claim (3 yr Lumpsum / Outright Pension)</div>
     <div class="collapse-content">
-        <a href="#" class="font-bold text-blue-600 hover:underline">Download Table of Contents</a>
+        <a href="#" class="text-blue-600 hover:underline">Download Table of Contents</a>
         <p>1 Folder containing 2 copies of the following:</p>
         <details class="collapse bg-base-200 hover:collapse-open">
             <summary class="collapse-title text-xl font-medium">A. Letter of Intent addressed to the Regional Director</summary>
             <div class="collapse-content">
               <ul>
-                <li> <a href="#" class="font-bold text-blue-600 hover:underline">Watch Video Tutorial</a></li>
-                <li>Download File</li>
+                <li><a href="https://youtu.be/HR2rqZPsVT8?si=I-Km7SCZV5najWQn" target="_blank" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+                <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/New-Letter-of-Intent-2024-1.docx" class="hover:underline">Download File</a></li>
               </ul>
             </div>
         </details>
@@ -23,8 +23,8 @@
           <summary class="collapse-title text-xl font-medium">B. Latest Personnal Data Sheet with Picture</summary>
           <div class="collapse-content">
             <ul>
-              <li> <a href="#">Watch Video Tutorial</a></li>
-              <li>Download File</li>
+              <li><a href="https://youtu.be/eIQYg96T0pg?si=pgGwxjeBtN6jBDYl" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/18_Personal_Data_Sheet_upd_03July2018.xlsx" class="hover:underline">Download File</a></li>
             </ul>
           </div>
         </details>
@@ -32,8 +32,8 @@
           <summary class="collapse-title text-xl font-medium">C. Duly filled-out ATM Account Information and Photocopy of ATM</summary>
           <div class="collapse-content">
             <ul>
-              <li> <a href="#">Watch Video Tutorial</a></li>
-              <li>Download File</li>
+              <li><a href="https://youtu.be/JMso1mJymqo?si=ueuXAhf0ia9Zgkme" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/ATM-info.pdf" class="hover:underline">Download File</a></li>
             </ul>
           </div>
         </details>
@@ -41,7 +41,7 @@
           <summary class="collapse-title text-xl font-medium">D. Ombudsman Clearance – Original Copy (1) Certified True Copy (1)</summary>
           <div class="collapse-content">
             <ul>
-              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li><a href="#" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
               <li>Download File</li>
             </ul>
           </div>
@@ -50,7 +50,7 @@
           <summary class="collapse-title text-xl font-medium">E. Declaration of Pendency/Non-pendency of Case from Civil Service Commission – Original Copy (1) Certified True Copy (1)</summary>
           <div class="collapse-content">
             <ul>
-              <li> <a href="#">Watch Video Tutorial</a></li>
+              <li> <a href="#" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
               <li>Download File</li>
             </ul>
           </div>
@@ -59,8 +59,7 @@
           <summary class="collapse-title text-xl font-medium">F. Retirement Order</summary>
           <div class="collapse-content">
             <ul>
-              <li> <a href="#">Watch Video Tutorial</a></li>
-              <li>Download File</li>
+              <li><a href="https://youtu.be/7MR1GVTg6ek?si=Zu1rMoHzyK4E-1LR" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
             </ul>
           </div>
         </details>
@@ -68,8 +67,7 @@
           <summary class="collapse-title text-xl font-medium">G. Updated Statement of Service</summary>
           <div class="collapse-content">
             <ul>
-              <li> <a href="#">Watch Video Tutorial</a></li>
-              <li>Download File</li>
+              <li><a href="https://youtu.be/4qXR29ltVyE?si=CRGPzJkDreaVcKk3" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
             </ul>
           </div>
         </details>
@@ -77,8 +75,7 @@
           <summary class="collapse-title text-xl font-medium">H. Photocopy of Original (FO1) and Latest Appointment</summary>
           <div class="collapse-content">
             <ul>
-              <li> <a href="#">Watch Video Tutorial</a></li>
-              <li>Download File</li>
+              <li> <a href="https://youtu.be/lnIQGUiPmXI?si=XK-7cTzfUTCviyOT" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
             </ul>
           </div>
         </details>
@@ -86,8 +83,18 @@
           <summary class="collapse-title text-xl font-medium">I. Certificate of Non-pending Case (RIAS Clearance) with valid BFP ID</summary>
           <div class="collapse-content">
             <ul>
-              <li> <a href="#">Watch Video Tutorial</a></li>
-              <li>Download File</li>
+              <li> <a href="https://youtu.be/S6SUdG6q8wU?si=ESsDHu9Nvt-ym6o0" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">Additional Requirements by COA - 1 Original Copy Each</summary>
+          <div class="collapse-content">
+            <ul>
+              <li>J. Statement of Assets, Liabilities and Net worth (SALN)</li>
+              <li><a href="https://youtu.be/o9tSr3jrTds?si=Uk0BtuESoJ3vQmVZ" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li>K. No Money and Property Accountability with Affidavit of Undertaking</li>
+              <li><a href="https://youtu.be/G3dIsHiZ7YQ?si=Z_S7tLm03f4JO_uB" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
             </ul>
           </div>
         </details>
@@ -95,8 +102,61 @@
   </div>
 <div class="collapse collapse-plus bg-base-200">
     <input type="radio" name="my-accordion-3" />
-    <div class="collapse-title text-xl font-medium">Terminal Leave Claim</div>
+    <div class="collapse-title text-xl font-medium">Terminal/Commutation Leave Claim</div>
     <div class="collapse-content">
-      <p>hello</p>
+      <a href="#" class="text-blue-600 hover:underline">Download Table of Contents</a>
+        <p>1 Folder containing 2 copies of the following:</p>
+        <details class="collapse bg-base-200 hover:collapse-open">
+            <summary class="collapse-title text-xl font-medium">A. Letter of Intent addressed to the Regional Director</summary>
+            <div class="collapse-content">
+              <ul>
+                <li><a href="https://youtu.be/HR2rqZPsVT8?si=I-Km7SCZV5najWQn" target="_blank" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+                <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/New-Letter-of-Intent-2024-1.docx" class="hover:underline">Download File</a></li>
+              </ul>
+            </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">B. Latest Personnal Data Sheet with Picture</summary>
+          <div class="collapse-content">
+            <ul>
+              <li><a href="https://youtu.be/eIQYg96T0pg?si=pgGwxjeBtN6jBDYl" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/18_Personal_Data_Sheet_upd_03July2018.xlsx" class="hover:underline">Download File</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">C. Duly filled-out ATM Account Information and Photocopy of ATM</summary>
+          <div class="collapse-content">
+            <ul>
+              <li><a href="https://youtu.be/JMso1mJymqo?si=ueuXAhf0ia9Zgkme" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/ATM-info.pdf" class="hover:underline">Download File</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">D. Application for Leave signed by applicant and approved by the Regional Director</summary>
+          <div class="collapse-content">
+            <ul>
+              <li><a href="https://www.youtube.com/watch?v=OPCQhTiduJQ&t=152s" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">E. No Money and Property Accountability Clearance</summary>
+          <div class="collapse-content">
+            <ul>
+              <li>NMPA Form of Last Unit Assignment (5 copies)</li>
+              <li><a href="https://youtu.be/G3dIsHiZ7YQ?si=RVsOMH4lZvdOX0KJ" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/LSS-19F-NO-MONEY-AND-PROPERTY-ACCOUNTABILITY-REGION-REV00.xlsx" class="hover:underline">Download File</a></li>
+              <li>Certificate of Badge Return (2 copies)</li>
+              <li><a href="https://youtu.be/VOuBp_8_-aM?si=5F_XeSB6lsYhOu-z" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li>Affidavit of Undertaking (3 copies)</li>
+              <li><a href="https://youtu.be/mDncBUC6PYw?si=sfAGbV_9RhJl79RW" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/Undertaking.docx" class="hover:underline">Download File</a></li>
+              <li>SAO Forms from previously assigned stations (2 copies)</li>
+              <li>Regional Level Service Record for Logistics Clearance (3 copies)</li>
+            </ul>
+          </div>
+        </details>
     </div>
 </div>
