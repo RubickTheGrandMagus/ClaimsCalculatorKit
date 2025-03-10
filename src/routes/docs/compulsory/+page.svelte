31dcb9160888c737e7ed0ab3faa361233d7e4724
dimz <derauld.media@gmail.com>
2025-03-10 20:28:25 +0800

    Final Part documentary requirements in compulsory retirees. Complete

--- src/routes/docs/compulsory/+page.svelte
+++ src/routes/docs/compulsory/+page.svelte
@@ -8,7 +8,7 @@
     <input type="radio" name="my-accordion-3" />
     <div class="collapse-title text-xl font-medium">Retirement Gratuity Claim (3 yr Lumpsum / Outright Pension)</div>
     <div class="collapse-content">
-        <a href="#" class="text-blue-600 hover:underline">Download Table of Contents</a>
+        <a href="https://region7.bfp.gov.ph/wp-content/uploads/TABLE-OF-CONTENTS-Gratuity.docx" class="text-blue-600 hover:underline">Download Table of Contents</a>
         <p>1 Folder containing 2 copies of the following:</p>
         <details class="collapse bg-base-200 hover:collapse-open">
             <summary class="collapse-title text-xl font-medium">A. Letter of Intent addressed to the Regional Director</summary>
@@ -39,21 +39,9 @@
         </details>
         <details class="collapse bg-base-200 hover:collapse-open">
           <summary class="collapse-title text-xl font-medium">D. Ombudsman Clearance – Original Copy (1) Certified True Copy (1)</summary>
-          <div class="collapse-content">
-            <ul>
-              <li><a href="#" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
-              <li>Download File</li>
-            </ul>
-          </div>
         </details>
         <details class="collapse bg-base-200 hover:collapse-open">
           <summary class="collapse-title text-xl font-medium">E. Declaration of Pendency/Non-pendency of Case from Civil Service Commission – Original Copy (1) Certified True Copy (1)</summary>
-          <div class="collapse-content">
-            <ul>
-              <li> <a href="#" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
-              <li>Download File</li>
-            </ul>
-          </div>
         </details>
         <details class="collapse bg-base-200 hover:collapse-open">
           <summary class="collapse-title text-xl font-medium">F. Retirement Order</summary>
@@ -104,7 +92,7 @@
     <input type="radio" name="my-accordion-3" />
     <div class="collapse-title text-xl font-medium">Terminal/Commutation Leave Claim</div>
     <div class="collapse-content">
-      <a href="#" class="text-blue-600 hover:underline">Download Table of Contents</a>
+      <a href="https://region7.bfp.gov.ph/wp-content/uploads/TABLE-OF-CONTENTS-Terminal.docx" class="text-blue-600 hover:underline">Download Table of Contents</a>
         <p>1 Folder containing 2 copies of the following:</p>
         <details class="collapse bg-base-200 hover:collapse-open">
             <summary class="collapse-title text-xl font-medium">A. Letter of Intent addressed to the Regional Director</summary>
@@ -145,18 +133,79 @@
           <summary class="collapse-title text-xl font-medium">E. No Money and Property Accountability Clearance</summary>
           <div class="collapse-content">
             <ul>
-              <li>NMPA Form of Last Unit Assignment (5 copies)</li>
+              <li>NMPA Form - Last Unit Assignment (5 copies)</li>
               <li><a href="https://youtu.be/G3dIsHiZ7YQ?si=RVsOMH4lZvdOX0KJ" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
               <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/LSS-19F-NO-MONEY-AND-PROPERTY-ACCOUNTABILITY-REGION-REV00.xlsx" class="hover:underline">Download File</a></li>
-              <li>Certificate of Badge Return (2 copies)</li>
+              <li>Certificate of Badge Return -  Last Unit Assignment (3 copies)</li>
               <li><a href="https://youtu.be/VOuBp_8_-aM?si=5F_XeSB6lsYhOu-z" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
-              <li>Affidavit of Undertaking (3 copies)</li>
+              <li>Affidavit of Undertaking - Last Unit Assignment (3 copies)</li>
               <li><a href="https://youtu.be/mDncBUC6PYw?si=sfAGbV_9RhJl79RW" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
               <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/Undertaking.docx" class="hover:underline">Download File</a></li>
-              <li>SAO Forms from previously assigned stations (2 copies)</li>
-              <li>Regional Level Service Record for Logistics Clearance (3 copies)</li>
+              <li>SAO Forms - previously assigned stations (3 copies)</li>
+              <li>Service Record - Last Unit Assignment (1 copy)</li>
+              <li>Retirement Order (1 copy)</li>
             </ul>
           </div>
         </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">F. Statement of Assets, Liabilities and Net worth (SALN)</summary>
+          <div class="collapse-content">
+            <ul>
+              <li><a href="https://youtu.be/o9tSr3jrTds?si=Uk0BtuESoJ3vQmVZ" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+              <li><a href="https://region7.bfp.gov.ph/wp-content/uploads/Template.doc" class="hover:underline">Download File</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">G. Commutation of Accumulated Leave Special Order</summary>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">H. Retirement Order</summary>
+          <div class="collapse-content">
+            <ul>
+              <li><a href="https://youtu.be/7MR1GVTg6ek?si=Zu1rMoHzyK4E-1LR" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">I. Updated Statement of Service</summary>
+          <div class="collapse-content">
+            <ul>
+              <li><a href="https://youtu.be/4qXR29ltVyE?si=CRGPzJkDreaVcKk3" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">J. Photocopy of Original (FO1) and Latest Appointment</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="https://youtu.be/lnIQGUiPmXI?si=XK-7cTzfUTCviyOT" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">K. Itemized Leave Record - Reconciled by NHQ</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="https://youtu.be/OPCQhTiduJQ?si=iFiPEjUGJLwEGJ9E" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">L & M. Certificate of Deletion and Last Payment - Issued by NHQ Finance</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="https://www.youtube.com/watch?v=OPCQhTiduJQ&t=247s" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+            </ul>
+          </div>
+        </details>
+        <details class="collapse bg-base-200 hover:collapse-open">
+          <summary class="collapse-title text-xl font-medium">N. Leave Card - Issued by Last Unit Assignment (1 copy - Handwritten)</summary>
+          <div class="collapse-content">
+            <ul>
+              <li> <a href="https://www.youtube.com/watch?v=OPCQhTiduJQ&t=205s" class="text-blue-600 hover:underline">Watch Video Tutorial</a></li>
+            </ul>
+          </div>
+        </details>
+      </div>
     </div>
-</div>
