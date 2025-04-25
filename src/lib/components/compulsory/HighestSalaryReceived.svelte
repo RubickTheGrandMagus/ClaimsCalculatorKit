bb39c5a7fb9a60b7f9fc93712fe79ce71be7b868
dimz <derauld@icloud.com>
2025-04-25 20:45:10 +0800

    Minor Fix: pagi on HSR component updated to no more itenerary operator.

--- src/lib/components/compulsory/HighestSalaryReceived.svelte
+++ src/lib/components/compulsory/HighestSalaryReceived.svelte
@@ -406,7 +406,7 @@
         rank:(HighestSalaryReceived.rank!="")? HighestSalaryReceived.rank:"",
         retrank:(HighestSalaryReceived.retrank!="")? HighestSalaryReceived.retrank:"Select Your Rank",
         bp:(HighestSalaryReceived.bp!=0)? HighestSalaryReceived.bp:0,
-        pagi:(HighestSalaryReceived.pagi!=0)? HighestSalaryReceived.pagi:Math.floor(YearsInSvc.allService.bfp.years/5),
+        pagi:Math.floor(YearsInSvc.allService.bfp.years/5),
         lp:(HighestSalaryReceived.lp!=0)? HighestSalaryReceived.lp:0,
         hsr:(HighestSalaryReceived.hsr!=0)? HighestSalaryReceived.hsr:0
     });
