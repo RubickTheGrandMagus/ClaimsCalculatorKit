eb7d045ba9c0d609da5cf7b735eb68dba305341a
dimz <derauld@icloud.com>
2025-04-30 09:43:58 +0800

    Minor Update: added redirects to death, tppd, mone

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,7 +4,7 @@ import { redirect } from '@sveltejs/kit';
 export function handle({ event, resolve }) {
     const path = event.url.pathname;
     
-    if (['/mc', '/mc/','/faq','/faq/','/bugreport','/bugreport/','/about','/about/'].includes(path)) {
+    if (['/mc', '/mc/','/faq','/faq/','/bugreport','/bugreport/','/about','/about/','/docs/mone','/docs/other','/docs/tppd','/docs/death'].includes(path)) {
         throw redirect(307, '/error');
     }
     
