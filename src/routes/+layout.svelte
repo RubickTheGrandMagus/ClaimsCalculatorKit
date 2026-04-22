9442698c66da1f2bdca31a7accb54f18cf99a3fb
RickSanchez <derauld@icloud.com>
2026-04-22 14:56:47 +0800

    Alert on homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -58,8 +58,12 @@
       </ul>
     </div>
   </div>
-<footer class="fixed bottom-0 left-0 sm:relative footer footer-center text-base-content text-xs p-4">
-  <aside>
-    <p>Copyright © 2025 - BFP Region 7 (Central Visayas)</p>
+  <footer class="fixed bottom-0 left-0 sm:relative footer footer-center text-base-content text-xs p-4">
+    <aside>
+      <div class="alert alert-info">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+          <span>Salary Table updated for CY 2026, 2027, and 2028.</span>
+        </div>
+    <p>Copyright © 2026 - BFP Region 7 (Central Visayas)</p>
   </aside>
 </footer>
